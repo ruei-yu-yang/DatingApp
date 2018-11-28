7fc349bb375817c6cdbcb4f6d2ad207b45d1e3e9
Ruei Yu <vip8209@yahoo.com.tw>
2018-11-28 14:34:59 +0800

    setting up the paing helper classes

---
+++ Helpers/UserParams.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Helpers
+{
+    public class UserParams
+    {
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        public int pageSize = 10;
+        public int MyProperty
+        {
+            get { return pageSize = 10; }
+            set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
+        }
+    }
+}
